6766feb7aeb80896a1a97653829b9e9f1ca3d2cb
Mihail <m.a.stepanov@outlook.com>
2025-03-31 12:58:48 +0300

    solved MergeSorted

--- MergeSorted.ts
+++ MergeSorted.ts
@@ -1,7 +1,13 @@
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
     if (n == 0) return;
-    if (m == 0) {
-        nums1.push(...nums2);
-        return;
+
+    nums1.splice(m);
+
+    let i = 0;
+    while (nums2.length > 0) {
+        if (i >= nums1.length || nums2[0] < nums1[i]) {
+            nums1.splice(i, 0, nums2.shift()!);
+        }
+        i++;
     }
 };
